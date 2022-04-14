d946e896d75758a44b731ce9d95241a684a6ebb1
Fraser McIntosh <frasermcintosh1@gmail.com>
2022-04-14 20:17:08 +1200

    Switch to a reducer

--- vanilla-react-wordle/src/App.jsx
+++ vanilla-react-wordle/src/App.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import { useState, useEffect, useReducer } from "react";
 import "./App.css";
 import Grid from "./components/Grid";
 import HeaderBar from "./components/HeaderBar";
@@ -13,62 +12,97 @@ const isLetter = (keyValue) => {
   return keyValue.length === 1 && /[a-zA-Z]/.test(keyValue);
 };
 
+const initialState = {
+  finite: {
+    view: {
+      value: "game", // game | settings | stats
+    },
+    core: {
+      value: "guessing", //  guessing | checkingWord
+    },
+  },
+  extended: {
+    targetWord: "LIVER",
+    currentGuess: [],
+    guesses: [],
+    wordTooShort: false,
+  },
+};
+
+function reducer(state, event) {
+  // this sort of direct access to state is undesirable
+  // ideally it'd be getter/setter only
+  const { finite, extended } = state;
+  const { wordTooShort, currentGuess, targetWord, guesses } = state.extended;
+  switch (event.type) {
+    case "KEY_EVENT":
+      const { keyValue } = event;
+      const newExtendedState = { ...state.extended };
+      console.log("current guess", currentGuess);
+      if (wordTooShort) {
+        // do nothing while we are animating
+        return state;
+      }
+
+      if (keyValue === "Enter") {
+        console.log(currentGuess.length);
+        if (currentGuess.length === WORD_LENGTH) {
+          // submit
+          if (currentGuess.join("") === targetWord) {
+            // correct
+            newExtendedState.correct = true;
+          } else {
+            // incorrect
+            if (guesses.length + 1 === MAX_GUESSES) {
+              // game over
+            }
+          }
+
+          newExtendedState.guesses = [
+            ...newExtendedState.guesses,
+            currentGuess,
+          ];
+        } else {
+          // too short
+          newExtendedState.wordTooShort = true;
+
+          // how do I set this timeout here?
+          // setTimeout(() => {
+          //   setWordTooShort(false);
+          // }, 1000);
+        }
+      } else if (keyValue === "Delete") {
+        if (currentGuess.length > 0) {
+          let newGuess = [...newExtendedState.currentGuess];
+          newGuess = [...newGuess.splice(0, newGuess.length - 1)];
+          newExtendedState.currentGuess = newGuess;
+        }
+      } else if (isLetter(keyValue)) {
+        if (currentGuess.length < WORD_LENGTH) {
+          const newGuess = [
+            ...newExtendedState.currentGuess,
+            getKeyDisplayFromKeyValue(keyValue),
+          ];
+          newExtendedState.currentGuess = newGuess;
+        }
+      }
+      return { finite, extended: newExtendedState };
+    case "DISABLE_WORD_TOO_SHORT":
+      return { ...state, wordTooShort: "false" };
+    default:
+      throw new Error();
+  }
+}
+
 const WORD_LENGTH = 5;
 const MAX_GUESSES = 6;
+
 function App() {
-  const [targetWord] = useState("HEART");
-  const [count, setCount] = useState(0);
-  const [currentGuess, setCurrentGuess] = useState([]);
-  const [guesses, setGuesses] = useState([]);
-  const [wordTooShort, setWordTooShort] = useState(false);
-  const [revealing, setRevealing] = useState(false);
   const [correct, setCorrect] = useState(false);
-
+  const [state, send] = useReducer(reducer, initialState);
+  const { currentGuess, guesses, wordTooShort } = state.extended;
   const handleKeyPress = (keyValue) => {
-    console.log("current guess", currentGuess);
-    if (wordTooShort) {
-      // do nothing while we are animating
-      return;
-    }
-
-    if (keyValue === "Enter") {
-      console.log(currentGuess.length);
-      if (currentGuess.length === WORD_LENGTH) {
-        // submit
-        if (currentGuess.join("") === targetWord) {
-          // correct
-          setCorrect(true);
-        } else {
-          // incorrect
-          if (guesses.length + 1 === MAX_GUESSES) {
-            // game over
-          }
-        }
-
-        setGuesses((guesses) => [...guesses, currentGuess]);
-        // setCurrentGuess([]);
-      } else {
-        // too short
-        setWordTooShort(true);
-        setTimeout(() => {
-          setWordTooShort(false);
-        }, 1000);
-      }
-    } else if (keyValue === "Delete") {
-      if (currentGuess.length > 0) {
-        setCurrentGuess((guess) => {
-          const newGuess = [...guess];
-          return [...newGuess.splice(0, guess.length - 1)];
-        });
-      }
-    } else if (isLetter(keyValue)) {
-      if (currentGuess.length < WORD_LENGTH) {
-        setCurrentGuess((guess) => [
-          ...guess,
-          getKeyDisplayFromKeyValue(keyValue),
-        ]);
-      }
-    }
+    send({ type: "KEY_EVENT", keyValue });
   };
 
   useEffect(() => {
